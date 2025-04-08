2a5525df3c577661592f7683c05c905b0dea58fe
deepajadhav1107 <deepajadhav2001@gmail.com>
2025-04-08 22:12:54 +0530

    save

---
+++ A.java
@@ -0,0 +1,11 @@
+package java6;
+
+public class A {
+        public static void main(String[] args)
+        {
+            System.out.println("Shrusti");
+            System.out.println("Deepa";)
+            System.out.println("MASTER";)
+        }
+
+}
